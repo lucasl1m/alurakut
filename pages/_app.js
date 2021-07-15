7259a25ca08987e1697b6d3279771dac95f67142
Lucas Araújo de Lima <70279700+lucasarlim@users.noreply.github.com>
2021-07-15 00:26:06 -0300

    Created MainGrid component and styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,31 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-`
+    font-family: sans-serif; 
+    background-color: #D9E6F6; }
+
+#__next {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +35,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import styled from "styled-components";
+import MainGrid from "../src/components/MainGrid";
+const Box = styled.div`
+  border-radius: 8px;
+  background-color: #ffff;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: "profileArea" }}>
+        <Box>
+          <img src='https://github.com/lucasarlim.png' 
+          />
+        </Box>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+        <Box></Box>
+      </div>
+      <div className style={{ gridArea: "profileRelationsArea" }}>
+        <Box>
+
+        </Box>
+      </div>
+    </MainGrid>
+  );
 }
